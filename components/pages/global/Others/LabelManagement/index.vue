69f692f6453aa4026443f12aacaa902a5fe2bcfe
BETASOFT\s.nahak <s.nahak@rescho.co.jp>
2023-06-02 17:23:01 +0900

    Others menu created

---
+++ components/pages/global/Others/LabelManagement/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="labelmanagement">
+        <PagesGlobalBaseLayout 
+            page-title="入院患者一覧" 
+        >
+            <template #iconImg>
+                <img :src="imageUrl"/> 
+            </template>
+            
+            <PagesGlobalOthersLabelManagementSearchField /> 
+        </PagesGlobalBaseLayout>
+    </div>
+</template>
+  
+<script setup lang="ts">
+import imageUrl from '@/assets/icon/ward.svg'
+</script>
+  
+<style lang="scss" scoped>
+.labelmanagement {
+    //width: 100%;
+    margin-left: 2%;
+    margin-top: -12px;
+}
+</style>
---
+++ pages/global/others/labelManagement/index.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+</script>
+
+<template>
+    <NuxtLayout name="global">
+        <div class="page-LabelManagement">
+            <PagesGlobalOthersLabelManagement />
+        </div>
+    </NuxtLayout>
+</template>
+  
+
+  
+  <style lang="scss" scoped>
+  .page-LabelManagement {
+    padding-bottom: 150px;
+    text-align: center !important;
+    background-color: #f0f0f0;
+  }
+  </style>
