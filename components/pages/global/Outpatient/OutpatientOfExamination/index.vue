b23eb110d4588ed88e4e28751caf1d36003b14e1
BETASOFT\s.nahak <s.nahak@rescho.co.jp>
2023-06-23 11:18:09 +0900

    outpatient background color set

--- components/pages/global/Outpatient/OutpatientOfExamination/index.vue
+++ components/pages/global/Outpatient/OutpatientOfExamination/index.vue
@@ -1,24 +1,49 @@
-<script setup lang="ts"> 
+<script setup lang="ts">
 import imageUrl from '@/assets/icon/schedule.svg'
 </script>
 <template>
-    <div class="outpatient-card-width">
-        <PagesGlobalGeneralGlobalBaseLayout
-            page-title="外来患者一覧（診察）"
-        >
-            <template #iconImg>
-                <img :src="imageUrl"/> 
-            </template>
-            <PagesGlobalOutpatientOutpatientOfExaminationSearchField />
-            <v-divider class="my-2 mt-15 ml-9 mr-9" />
-            <PagesGlobalOutpatientOutpatientOfExaminationConsultationProgress />
-            <PagesGlobalOutpatientOutpatientOfExaminationTable />
-        </PagesGlobalGeneralGlobalBaseLayout>
+  <div class="outpatient-card-width">
+    <GeneralGlobalHeader page-title="外来患者一覧（診察）">
+      <template #iconImg>
+        <img :src="imageUrl" />
+      </template>
+    </GeneralGlobalHeader>
+    <div class="contents">
+      <div class="contents-search-section">
+        <PagesGlobalOutpatientOutpatientOfExaminationSearchField />
+        <v-divider class="my-2 mt-15 ml-9 mr-9" />
+        <PagesGlobalOutpatientOutpatientOfExaminationConsultationProgress />
+      </div>
+      <div class="contents-table-section">
+        <PagesGlobalOutpatientOutpatientOfExaminationTable />
+      </div>
     </div>
+  </div>
 </template>
 <style scoped>
 .outpatient-card-width {
-    margin-left: 2%;
-    margin-top: -12px;
+  margin-left: 2%;
+  margin-top: -12px;
+}
+.contents {
+  /* display: flex; */
+  height: calc(100vh - 60px);
+  width: 100% !important;
+  border-radius: 10px !important;
+  background: white;
+
+}
+.contents-search-section {
+  width: 99.5%;
+  margin: 0px;
+  border-radius: 10px !important;
+  background: white;
+  /* text-align: center; */
+}
+.contents-table-section {
+  width: 99.5%;
+  border-radius: 10px !important;
+  margin: 0px;
+  background: white;
 }
 </style>
